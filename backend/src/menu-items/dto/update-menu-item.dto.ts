3b62fc2a5cca0549cea6813726185b82d104075e
i-zokirov <izokirov@outlook.com>
2023-02-19 23:07:27 +0100

    removed unnecessary module

--- backend/src/menu-items/dto/update-menu-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMenuItemDto } from './create-menu-item.dto';
-
-export class UpdateMenuItemDto extends PartialType(CreateMenuItemDto) {}
--- backend/src/menu-items/menu-items.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MenuItemsService } from './menu-items.service';
-import { MenuItemsController } from './menu-items.controller';
-
-@Module({
-  controllers: [MenuItemsController],
-  providers: [MenuItemsService]
-})
-export class MenuItemsModule {}
