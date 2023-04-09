94660169780450e10aea1510d26caa090a64fc04
i-zokirov <izokirov@outlook.com>
2023-04-09 15:19:55 +0200

    login and sign-up pages

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,8 +1,74 @@
+import Link from "next/link";
 import React from "react";
-import Main from "@/layouts/Main";
-import Meta from "@/layouts/Meta";
+
 const login = () => {
-    return <div>Login</div>;
+    return (
+        <section
+            className="login-page"
+            style={{
+                backgroundImage: "url(assets/img/background-1.png)",
+                position: "relative",
+            }}
+        >
+            <div className="row">
+                <div className="col-lg-6 col-md-12 d-flex align-items-center justify-content-center image-col"></div>
+                <div className="col-lg-6 col-md-12 d-flex align-items-center justify-content-center form-col">
+                    <div className="form-wrapper p-5">
+                        <h4>Sign in </h4>
+                        <form>
+                            <div className="mb-3">
+                                <label
+                                    htmlFor="email"
+                                    className="form-label text-start"
+                                >
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    aria-describedby="emailHelp"
+                                    placeholder="Enter email"
+                                />
+                                <div className="invalid-feedback text-end">
+                                    Please enter a valid email address.
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <label
+                                    htmlFor="password"
+                                    className="form-label text-start"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="password"
+                                    placeholder="Password"
+                                />
+                                <div className="invalid-feedback text-end">
+                                    Please enter a valid password.
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="button button-contained"
+                            >
+                                Login
+                            </button>
+                            <div className="mt-3">
+                                <span>
+                                    Not registered?{" "}
+                                    <Link href={"/sign-up"}>Sign up here</Link>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default login;
--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,36 +1,21 @@
-import Link from "next/link";
 import React from "react";
+import "./LoginPage.css";
 
-const signup = () => {
+const LoginPage = () => {
     return (
-        <section
-            className="register-page"
-            style={{
-                backgroundImage: "url(assets/img/background-1.png)",
-                position: "relative",
-            }}
-        >
+        <div className="container-fluid">
             <div className="row">
-                <div className="col-lg-6 col-md-12 d-flex align-items-center justify-content-center image-col"></div>
+                <div className="col-lg-6 col-md-12 d-flex align-items-center justify-content-center image-col">
+                    <img
+                        src="https://via.placeholder.com/500x500.png"
+                        alt="Login"
+                        className="img-fluid"
+                    />
+                </div>
                 <div className="col-lg-6 col-md-12 d-flex align-items-center justify-content-center form-col">
                     <div className="form-wrapper p-5">
-                        <h4>Sign Up</h4>
+                        <h2>Login</h2>
                         <form>
-                            <div className="mb-3">
-                                <label
-                                    htmlFor="name"
-                                    className="form-label text-start"
-                                >
-                                    Full name
-                                </label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="name"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Enter your name"
-                                />
-                            </div>
                             <div className="mb-3">
                                 <label
                                     htmlFor="email"
@@ -68,22 +53,30 @@ const signup = () => {
                             </div>
                             <button
                                 type="submit"
-                                className="button button-contained"
+                                className="btn btn-primary w-100"
                             >
-                                Sign Up
+                                Login
                             </button>
-                            <div className="mt-3">
-                                <span>
-                                    Already have an account?{" "}
-                                    <Link href={"/login"}>Sign in here</Link>
-                                </span>
-                            </div>
                         </form>
+                        <div className="social-buttons mt-3">
+                            <button
+                                type="button"
+                                className="btn btn-outline-primary me-2"
+                            >
+                                Continue with Google
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-outline-primary"
+                            >
+                                Continue with Telegram
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     );
 };
 
-export default signup;
+export default LoginPage;
